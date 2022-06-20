70f744593910c9fa0654b954038e3161c5a21bf0
Max Shapira <maxshapira9@gmail.com>
2022-06-20 19:10:36 +0300

    create ShortUrl model

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,3 +1,45 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class ShortUrl(models.Model):
+
+    key = models.CharField(
+        max_length=7,
+        primary_key=True,
+        editable=False,
+    )
+    long_url = models.URLField(verbose_name="Long URL")
+    short_url = models.CharField(max_length=100)
+    visits = models.PositiveIntegerField(default=0, verbose_name="Visits")
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created At",
+    )
+
+    def __str__(self):
+        # return f"{self.short_url} - {self.visits} visits."
+        return self.key
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def generate_key(cls) -> str:
+        key = uuid.uuid4().hex[:7]
+        count = 0
+        while cls.objects.filter(key=key).exists() or count > 7:
+            key = uuid.uuid4().hex[:7]
+            count += 1
+        return key
+
+    @property
+    def shorten_url(self):
+        return f"http://localhost:8000/s/{self.key}"
+
+    class Meta:
+        verbose_name = "link"
+        verbose_name_plural = "Most viewed links"
--- shorturls/views.py
+++ shorturls/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
+
+# from .models import ShortUrl
 
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+@csrf_exempt
+def short_url(request):
