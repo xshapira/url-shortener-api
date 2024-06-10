a336b6722dfc6130c850706fb8a7de94ccb29870
Max Shapira <max.shapira@icloud.com>
2024-06-10 10:59:43 +0300

    add ShortUrl model to store shortened urls

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+from django.db import models
+
+
+class ShortUrl(models.Model):
+    """ShortUrl Model Definition"""
+
+    key = models.CharField(
+        max_length=7,
+        primary_key=True,
+        editable=False,
+        unique=True,
+    )
+    long_url = models.URLField(verbose_name="Long URL")
+    visits = models.PositiveIntegerField(default=0, verbose_name="Visits")
+
+    def __str__(self):
+        return self.key
