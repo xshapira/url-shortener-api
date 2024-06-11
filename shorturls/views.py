fc0ace2b190315ebcbfdd348bc4f1718b42b5ccc
Max Shapira <max.shapira@icloud.com>
2024-06-11 15:02:05 +0300

    replace HomeView with direct TemplateView in urls.py

--- shorturls/views.py
+++ shorturls/views.py
@@ -7,13 +7,8 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from django.views.generic import TemplateView
 
 from shorturls.models import ShortUrl
-
-
-class HomeView(TemplateView):
-    template_name = "index.html"
 
 
 @method_decorator(csrf_exempt, name="dispatch")
