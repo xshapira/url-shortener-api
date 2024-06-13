17689785f60bf6643fc524ddbd7df898b74af3cb
Max Shapira <max.shapira@icloud.com>
2024-06-13 12:34:01 +0300

    display created_at field as read-only and list view in admin

--- shorturls/admin.py
+++ shorturls/admin.py
@@ -6,8 +6,5 @@ from shorturls.models import ShortUrl
 @admin.register(ShortUrl)
 class ShortUrlAdmin(admin.ModelAdmin):
     ordering = ("-visits",)
-    list_display = (
-        "__str__",
-        "visits",
-        "created_at",
-    )
+    list_display = ("__str__", "visits", "created_at")
+    readonly_fields = ("created_at",)
