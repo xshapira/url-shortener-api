b147e46c7a708d87e4eb9bbca1799b60aeb27543
Max Shapira <max.shapira@icloud.com>
2024-06-14 11:20:14 +0300

    display creation time with seconds precision in django admin

--- shorturls/admin.py
+++ shorturls/admin.py
@@ -5,6 +5,19 @@ from shorturls.models import ShortUrl
 
 @admin.register(ShortUrl)
 class ShortUrlAdmin(admin.ModelAdmin):
-    ordering = ("-visits",)
-    list_display = ("__str__", "visits", "created_at")
-    readonly_fields = ("created_at",)
+    ordering = ("-visits", "-created_at")
+    list_display = (
+        "__str__",
+        "visits",
+        "created_at",
+        "created_time",
+    )
+    readonly_fields = ("created_at", "created_time")
+
+    @admin.display(description="Created Time (ss)")
+    def created_time(self, obj):
+        return (
+            obj.created_at.strftime("%-I:%M:%S %p")
+            .replace("AM", "a.m.")
+            .replace("PM", "p.m.")
+        )
